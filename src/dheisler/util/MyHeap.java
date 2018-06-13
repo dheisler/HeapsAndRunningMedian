174ef7cccccbd8378044a14a1d05e7ee35a57912
Debbie Heisler <debbieheisler@gmail.com>
2018-06-13 14:42:18 -0700

    Setting up Intellij directory

---
+++ src/dheisler/util/MyHeap.java
@@ -0,0 +1,10 @@
+/**
+ * This implementation of a heap is based on the algorithms for Heapsort in
+ * Introduction to Algorithms by Cormen, Leiserson, Rivest
+ */
+
+package dheisler.util;
+
+public class MyHeap
+{
+}
---
+++ test/dheisler/util/MyHeapTest.java
@@ -0,0 +1,8 @@
+package dheisler.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyHeapTest
+{
+
+}
