a78badfed21621b84c390231137c8687e377dc02
Debbie Heisler <debbieheisler@gmail.com>
2018-06-19 15:38:00 -0700

    Wrote and tested getting parent/child index from given index:

--- src/dheisler/util/MyHeap.java
+++ src/dheisler/util/MyHeap.java
@@ -1,10 +1,37 @@
 /**
  * This implementation of a heap is based on the algorithms for Heapsort in
  * Introduction to Algorithms by Cormen, Leiserson, Rivest
+ *
+ * It was slightly modified so that it it could handle a 0 based array instead
+ * of a 1 based array.
  */
 
 package dheisler.util;
 
+import java.util.ArrayList;
+
 public class MyHeap
 {
+    private ArrayList<Integer> myArray;
+
+    public MyHeap()
+    {
+        myArray = new ArrayList<Integer>();
+    }
+
+
+    protected int getParentIndex(int index)
+    {
+        return (index - 1)/2;
+    }
+
+    protected int getLeftChildIndex(int index)
+    {
+        return (2 * index) + 1;
+    }
+
+    protected int getRightChildIndex(int index)
+    {
+        return (2 * index) + 2;
+    }
 }
--- test/dheisler/util/MyHeapTest.java
+++ test/dheisler/util/MyHeapTest.java
@@ -1,8 +1,23 @@
 package dheisler.util;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyHeapTest
 {
+
+    @Test
+    public void testGettingParentChildIndices()
+    {
+        MyHeap myHeap = new MyHeap();
+        assertEquals(0, myHeap.getParentIndex(1));
+        assertEquals(0, myHeap.getParentIndex(2));
+        assertEquals(4, myHeap.getParentIndex(10));
+
+        assertEquals(1, myHeap.getLeftChildIndex(0));
+        assertEquals(2, myHeap.getRightChildIndex(0));
+        assertEquals(7, myHeap.getLeftChildIndex(3));
+    }
 
 }
